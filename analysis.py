d11870f25db0d951064908c6a9bb671fcdf5a08b
adole2008 <doleanushka@gmail.com>
2025-09-20 18:25:31 -0400

    cell clusters and genes of interest notebooks added for patient 12 and patient 4 tumor and control samples

--- analysis.py
+++ analysis.py
@@ -1,66 +1,8 @@
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import matplotlib.pyplot as plt
 
-
-# lines 6 - 29 from: https://www.danli.org/2021/02/03/single-cell-data-analysis-using-scanpy/ 
-sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
-sc.logging.print_header()
-sc.settings.set_figure_params(dpi=80, facecolor='white')
-
-adata = sc.read_10x_mtx(
-    "datasets", 
-    var_names="gene_symbols",
-    cache=True
-)
-
-adata.var_names_make_unique()
-
-#filter 20 highest expressed genes
-#sc.pl.highest_expr_genes(adata, n_top=20, )
-
-#filtering out the cells with low gene expression/genes that don't show up in many cells
-sc.pp.filter_cells(adata, min_genes=200)
-sc.pp.filter_genes(adata, min_cells=3)
-
-#annotate mitochondrial genes as 'mt' and calculate qc metrics based on 
-adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-adata.var['ribo'] = adata.var_names.str.startswith(("RPS", "RPL"))
-adata.var['hb'] = adata.var_names.str.startswith("^HB[^(P)]") #regular expression that chooses all leters after HB besides the capital letter P
-#regex is used because pseudogenes have names like HBP1, HBP2, etc 
-
-sc.pp.calculate_qc_metrics(adata, qc_vars=["mt", "ribo", "hb"], percent_top=None, log1p=False, inplace=True)
-
-sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
-             jitter=0.4, multi_panel=True)
-
-sc.pl.scatter(adata, "total_counts", "n_genes_by_counts", color="pct_counts_mt")
-sc.pl.scatter(adata, 'total_counts', 'pct_counts_mt')
-
-adata_cp1 = adata[
-    (adata.obs['pct_counts_mt'] < 10) &
-    (adata.obs['pct_counts_ribo'] < 20) &
-    (adata.obs['pct_counts_hb'] < 5),
-    :
-].copy()
-
-sc.pl.violin(adata_cp1, ['pct_counts_mt', 'pct_counts_ribo', 'pct_counts_hb'], jitter=0.4)
-
-#Doublet Detection
-adata_cp2 = adata_cp1.copy()
-sc.pp.scrublet(adata_cp2)
-
-sc.pp.neighbors(adata_cp2)  
-sc.tl.umap(adata_cp2)   
-sc.pl.umap(adata_cp2, color='doublet_score', color_map='viridis', show=True)
-plt.show()
-
-#doublet detection - 1 peak is good, 2 peaks is an outside effect that is impacting the doublets
-#sc.pp.scrublet(adata, batch_key="sample")
-
-# wide part of the violin plot of after doublet filtering should be higher than the mean of the before plot
-
-#filtered highly variable genes
-#filtered all genes -> input into the model, QC filtered adata_normlog is where you would run the model
-#.write_h5ad("path") command
+meta = pd.read_csv("datasets/metadata.csv.gz")
+type(meta)
+meta.columns = ["cell_id", "tissue", "patient", "cell-type"]
+print([p for p in meta["tissue"].values])
