cc88786f75ab5cd0d773133116d1e23414c51ac2
adole2008 <doleanushka@gmail.com>
2025-10-14 17:00:32 -0400

    using tonsils as a control for spatial, goal is to locate and annotate TLS's in the spatial data

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import scanpy as sc
-
-meta = pd.read_csv("datasets/metadata.csv.gz")
-type(meta)
-meta.columns = ["cell_id", "tissue", "patient", "cell-type"]
-print([p for p in meta["tissue"].values])
